57ee20b89fc6db215be23928d55af65ee834e730
Qenawi <aldmanmero@yahoo.com>
2020-08-21 11:53:18 +0200

    Initial Commit

---
+++ news_app_medium/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  news_app_medium
+//
+//  Created by Qenawi on 8/21/20.
+//  Copyright © 2020 Qenawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
